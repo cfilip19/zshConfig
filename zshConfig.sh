a3a3f23be7a7ff6bece53526766f4d8a5c9f3d3d
cfilip19 <c_filip19@yahoo.com>
2022-07-26 10:18:44 +0300

    Update zshConfig.sh

--- zshConfig.sh
+++ zshConfig.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo yum install zsh -y
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh --unattended)"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="candy"/' ~/.zshrc
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions)/' ~/.zshrc
